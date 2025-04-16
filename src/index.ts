af652cbfec3eedd5fc50a9230809fe5809111d4a
Corentin Tournier <corentin.tournier@ikigaifreelance.com>
2025-04-16 11:12:25 +0200

    Application setup #3

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,43 @@
 import express from "express"
 import "reflect-metadata";
+import { ApolloServer } from "@apollo/server";
+import http from "http";
+import datasource from "./lib/datasource";
+import 'dotenv/config';
 
 const app = express();
+const httpServer = http.createServer(app);
 
-app.use(express.json());
+const typeDefs = `
+  type Query {
+    hello: String
+  }
+`;
 
-app.listen(4000, () => {
-  console.log("Le serveur est lancé sur le port 4000");
-});
+const resolvers = {
+  Query: {
+    hello: () => "Hello, world!",
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+})
+
+async function main() {
+  await server.start();
+  console.log("Apollo Server démarré");
+
+  await datasource.initialize();
+
+
+  app.listen(4000, () => {
+    console.log("Le serveur est lancé sur le port 4000");
+  });
+
+  app.use(express.json());
+}
+
+main();
+
