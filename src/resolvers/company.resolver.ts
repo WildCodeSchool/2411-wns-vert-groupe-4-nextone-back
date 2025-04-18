03016dd738588ff69f997615258be4e50bb83c6f
William Mibelli <william.mibelli@gmail.com>
2025-04-18 12:01:39 +0200

    Company entity, resolver et service, + baseService

--- src/resolvers/company.resolver.ts
+++ src/resolvers/company.resolver.ts
@@ -1,9 +1,18 @@
+<<<<<<< HEAD
 import {
   DeleteResponseCompany,
   MutationCreateCompanyArgs,
   MutationDeleteCompanyArgs,
   MutationUpdateCompanyArgs,
   QueryCompanyArgs,
+=======
+
+import {
+  CreateCompanyInput,
+  MutationCreateOneArgs,
+  MutationDeleteOneArgs,
+  QueryFindByIdArgs,
+>>>>>>> e7b1fda (Company entity, resolver et service, + baseService)
 } from "@/generated/graphql";
 import { MyContext } from "..";
 import CompanyService from "@/services/company.service";
@@ -13,6 +22,7 @@ const companyService = CompanyService.getService();
 
 export default {
   Query: {
+<<<<<<< HEAD
     companies: async (_: any): Promise<CompanyEntity[]> => {
       const companies = await companyService.findAll();
       return companies;
@@ -20,12 +30,22 @@ export default {
     company: async (
       _: any,
       { id }: QueryCompanyArgs
+=======
+    findAll: async (_: any): Promise<CompanyEntity[]> => {
+      const companies = await companyService.findAll();
+      return companies;
+    },
+    findById: async (
+      _: any,
+      { id }: QueryFindByIdArgs
+>>>>>>> e7b1fda (Company entity, resolver et service, + baseService)
     ): Promise<CompanyEntity | null> => {
       const company = await companyService.findById(id);
       return company;
     },
   },
   Mutation: {
+<<<<<<< HEAD
     createCompany: async (
       _: any,
       args: MutationCreateCompanyArgs,
@@ -64,5 +84,24 @@ export default {
       );
       return updatedCompany;
     },
+=======
+    createOne: async (
+      _: any,
+      args : MutationCreateOneArgs,
+      ctx: MyContext
+    ): Promise<CompanyEntity> => {
+      const test: Partial<CompanyEntity> = {...args.data}
+      const newCompany = await companyService.createOne(test);
+      return newCompany;
+    },
+    deleteOne: async (
+      _: any,
+      args : MutationDeleteOneArgs,
+      ctx: MyContext
+    ): Promise<boolean> => {
+      const isDeleted = await companyService.deleteOne(args.id)
+      return isDeleted
+    }
+>>>>>>> e7b1fda (Company entity, resolver et service, + baseService)
   },
 };
