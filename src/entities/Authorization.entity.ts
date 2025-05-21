b6d8e8501ac330275cd61bf101bc638fcc165d53
Maeva RODRIGUES <maeva.rodrigues@lapiscine.pro>
2025-05-21 12:03:42 +0200

    🎉 Initial Commit: create Authorization entity without relations

---
+++ src/entities/Authorization.entity.ts
@@ -0,0 +1,23 @@
+import {
+  Entity,
+  PrimaryColumn,
+  Column,
+  CreateDateColumn,
+} from 'typeorm';
+
+@Entity('authorizations')
+export default class Authorization {
+  @PrimaryColumn('uuid')
+  serviceId: string; // composite key
+
+  @PrimaryColumn('uuid')
+  managerId: string; // composite key
+
+  @Column({ default: true })
+  isActive: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
+
+  // Relations
