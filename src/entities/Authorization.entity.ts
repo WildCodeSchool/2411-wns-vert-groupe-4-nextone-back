91dee680765bf8f222f0eebb75a70eab078a67ed
Maeva RODRIGUES <maeva.rodrigues@lapiscine.pro>
2025-05-22 15:40:27 +0200

    💥 NEW: Add relations authorization + service entity

--- src/entities/Authorization.entity.ts
+++ src/entities/Authorization.entity.ts
@@ -3,7 +3,11 @@ import {
   PrimaryColumn,
   Column,
   CreateDateColumn,
+  ManyToOne,
+  JoinColumn,
 } from 'typeorm';
+import { ServiceEntity } from "./Service.entity";
+// import { ManagerEntity } from "./Manager.entity"; // commenté car pas encore créé
 
 @Entity('authorizations')
 export default class AuthorizationEntity {
@@ -18,6 +22,18 @@ export default class AuthorizationEntity {
 
   @CreateDateColumn()
   createdAt: Date;
-}
+
 
   // Relations
+ 
+  // Relation vers Service (ManyToOne)
+  @ManyToOne(() => ServiceEntity, (service) => service.authorizations)
+  @JoinColumn({ name: "serviceId" })
+  service: ServiceEntity;
+
+  // Relation vers Manager (ManyToOne) quand elle sera créée
+  // @ManyToOne(() => ManagerEntity, (manager) => manager.authorizations)
+  // @JoinColumn({ name: "managerId" })
+  // manager: ManagerEntity;
+
+}
