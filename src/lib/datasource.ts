a3e234a56b0d1a10a95a36e46890135ada8e73c2
BERTRAND <oceane.bertrand@cgi.com>
2025-06-27 16:47:31 +0200

    change datasource

--- src/lib/datasource.ts
+++ src/lib/datasource.ts
@@ -1,56 +1,30 @@
-// import { ServiceEntity } from "../entities/Service.entity";
-// import TicketEntity from "@/entities/Ticket.entity";
-// import CompanyEntity from "@/entities/Company.entity";
-// import AuthorizationEntity from "@/entities/Authorization.entity";
-// import { DataSource } from "typeorm";
-// import SettingEntity from "@/entities/setting.entity";
-// import TicketLogEntity from "@/entities/TicketLog.entity";
-// import ManagerEntity from "@/entities/Manager.entity";
-
-// export default new DataSource({
-//   type: "postgres",
-//   host: "db",
-//   port: 5432,
-//   username: process.env.POSTGRES_USER,
-//   password: process.env.POSTGRES_PASSWORD,
-//   database: process.env.POSTGRES_DB,
-//   entities: [
-    // TicketEntity,
-    // ServiceEntity,
-    // AuthorizationEntity,
-    // CompanyEntity,
-    // SettingEntity,
-    // TicketLogEntity,
-    // ManagerEntity
-//   ],
-//   logging: ["error"],
-//   synchronize: true,
-// });
-
-
-import ManagerEntity from "@/entities/Manager.entity";
-import { DataSource } from "typeorm";
 import { ServiceEntity } from "../entities/Service.entity";
 import TicketEntity from "@/entities/Ticket.entity";
 import CompanyEntity from "@/entities/Company.entity";
 import AuthorizationEntity from "@/entities/Authorization.entity";
+import { DataSource } from "typeorm";
 import SettingEntity from "@/entities/setting.entity";
 import TicketLogEntity from "@/entities/TicketLog.entity";
+import ManagerEntity from "@/entities/Manager.entity";
 
 export default new DataSource({
   type: "postgres",
-  database: "nextone",
-  username: "postgres",
-  password: "secret",
-  // entities: ["/src/entities/*.entities.ts"],
-  entities: [TicketEntity,
+  host: "db",
+  port: 5432,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  entities: [
+    TicketEntity,
     ServiceEntity,
     AuthorizationEntity,
     CompanyEntity,
     SettingEntity,
     TicketLogEntity,
-    ManagerEntity],
-  synchronize: true, // pas à utiliser en prod (faire des migrations pour la prod);
-  logging: false, // nous permettra de voir les requêtes SQL qui sont jouées dans le terminal
-  // logging: ["error", "query"], // nous permettra de voir les requêtes SQL qui sont jouées dans le terminal
+    ManagerEntity
+  ],
+  logging: ["error"],
+  synchronize: true,
 });
+
+
--- src/lib/datasource_test.ts
+++ src/lib/datasource_test.ts
@@ -1,61 +1,33 @@
-// import CompanyEntity from "@/entities/Company.entity";
-// import { DataSource } from "typeorm";
-// import TicketEntity from "@/entities/Ticket.entity";
-// import { ServiceEntity } from "@/entities/Service.entity";
-// import AuthorizationEntity from "@/entities/Authorization.entity";
-
-// import SettingEntity from "@/entities/setting.entity";
-// import ManagerEntity from "@/entities/Manager.entity";
-// import TicketLogEntity from "@/entities/TicketLog.entity";
-
-
-// export default new DataSource({
-//   type: "postgres",
-//   host: "localhost",
-//   port: 5436,
-//   username: process.env.POSTGRES_USER || "utilisateur",
-//   password: process.env.POSTGRES_PASSWORD || "password",
-//   database: "nextone-db-test",
-
-//   entities: [
-//     TicketEntity,
-//     CompanyEntity,
-//     ServiceEntity,
-//     AuthorizationEntity,
-//     SettingEntity,
-//     ManagerEntity,
-//     TicketLogEntity,
-//   ],
-
-//   synchronize: true, // pas à utiliser en prod (faire des migrations pour la prod);
-//   logging: ["error"], // nous permettra de voir les requêtes SQL qui sont jouées dans le terminal
-//   // logging: ["error", "query"], // nous permettra de voir les requêtes SQL qui sont jouées dans le terminal
-// });
-
-
-import ManagerEntity from "@/entities/Manager.entity";
-import { DataSource } from "typeorm";
-import { ServiceEntity } from "../entities/Service.entity";
-import TicketEntity from "@/entities/Ticket.entity";
 import CompanyEntity from "@/entities/Company.entity";
+import { DataSource } from "typeorm";
+import TicketEntity from "@/entities/Ticket.entity";
+import { ServiceEntity } from "@/entities/Service.entity";
 import AuthorizationEntity from "@/entities/Authorization.entity";
+
 import SettingEntity from "@/entities/setting.entity";
+import ManagerEntity from "@/entities/Manager.entity";
 import TicketLogEntity from "@/entities/TicketLog.entity";
+
 
 export default new DataSource({
   type: "postgres",
-  database: "nextone_test",
-  username: "postgres",
-  password: "secret",
-  // entities: ["/src/entities/*.entities.ts"],
-  entities: [TicketEntity,
+  host: "localhost",
+  port: 5436,
+  username: process.env.POSTGRES_USER || "utilisateur",
+  password: process.env.POSTGRES_PASSWORD || "password",
+  database: "nextone-db-test",
+
+  entities: [
+    TicketEntity,
+    CompanyEntity,
     ServiceEntity,
     AuthorizationEntity,
-    CompanyEntity,
     SettingEntity,
+    ManagerEntity,
     TicketLogEntity,
-    ManagerEntity],
+  ],
+
   synchronize: true, // pas à utiliser en prod (faire des migrations pour la prod);
-  logging: false, // nous permettra de voir les requêtes SQL qui sont jouées dans le terminal
+  logging: ["error"], // nous permettra de voir les requêtes SQL qui sont jouées dans le terminal
   // logging: ["error", "query"], // nous permettra de voir les requêtes SQL qui sont jouées dans le terminal
 });
