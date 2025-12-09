588bcd6dcaeaaad2ced7ed2907f86ac9912bc981
ocebert31 <oceanebertrand339@gmail.com>
2025-12-09 15:57:24 +0100

    fix context ip

--- src/lib/ipContext.ts
+++ src/lib/ipContext.ts
@@ -1,9 +1,26 @@
 import { Request } from "express";
+import os from 'os';
+
+function getServerLocalIPv4() {
+  const interfaces: any = os.networkInterfaces();
+  let localIP = null;
+  for (const name of Object.keys(interfaces)) {
+    for (const iface of interfaces[name]) {
+      if (iface.family === 'IPv4' && !iface.internal) {
+        localIP = iface.address;
+        return localIP;
+      }
+    }
+  }
+  return localIP || '127.0.0.1'; 
+}
+console.log('IP locale du serveur :', getServerLocalIPv4());
+
 
 export const ipContext = async ({ req }: { req: Request }) => {
   const ip =
+  getServerLocalIPv4() ||
     (req.headers["x-forwarded-for"] as string | undefined) ||
     (req.socket.remoteAddress as string);
-
   return { ip };
 };
